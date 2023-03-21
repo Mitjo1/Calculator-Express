e19b151b854ec04330c68d88b30563ddea782258
Milan <milan1adamovic@gmail.com>
2023-03-21 09:40:24 +0100

    Full funcionality added on back-end

--- calculator.js
+++ calculator.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000);
+app.post("/", function (req, res) {
+  var n1 = Number(req.body.num1);
+  var n2 = Number(req.body.num2);
+  var result = num1 + num2;
+
+  res.send("The result is " + result + ".");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000.");
+});
